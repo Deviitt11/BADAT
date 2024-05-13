1e987a7ada2ff5455cba9eee3d83f0d468e069f4
Deviitt11 <98158844+Deviitt11@users.noreply.github.com>
2024-05-13 21:28:33 +0200

    13 Mayo v2

--- Tema 7/Actividad_Tema7.sql
+++ Tema 7/Actividad_Tema7.sql
@@ -12,8 +12,24 @@ BEGIN
     -- y las inserto a pinchu en la tabla
     DELETE FROM tarro;
     INSERT INTO tarro (galleta_campo) VALUES (g1);
+    DBMS_OUTPUT.PUT_LINE('Galleta 1 insertada correctamente');
     INSERT INTO tarro (galleta_campo) VALUES (g2);
+    DBMS_OUTPUT.PUT_LINE('Galleta 2 insertada correctamente');
     INSERT INTO tarro (galleta_campo) VALUES (g3);
+    DBMS_OUTPUT.PUT_LINE('Galleta 3 insertada correctamente');
+    
+    -- pruebo métodos
+    g1.muestra();
+    g2.muestra();
+    g3.muestra();
+    
+    DBMS_OUTPUT.PUT_LINE('Precio galleta 1: ' || g1.precio(5.17, 7.25, 10.0) || ' euros');
+    DBMS_OUTPUT.PUT_LINE('Precio galleta 2: ' || g2.precio(2.33, 4.20, 8.8) || ' euros');
+    DBMS_OUTPUT.PUT_LINE('Precio galleta 3: ' || g3.precio(1.40, 2.00, 3.50) || ' euros');
+    
+    DBMS_OUTPUT.PUT_LINE('Masa total galleta 1: ' || g1.masatotal() || ' gramos');
+    DBMS_OUTPUT.PUT_LINE('Masa total galleta 2: ' || g2.masatotal() || ' gramos');
+    DBMS_OUTPUT.PUT_LINE('Masa total galleta 3: ' || g3.masatotal() || ' gramos');
 END;
 /
 
